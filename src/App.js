df56ccfb6995c544e85ce874976b580b1235a1aa
Robert S Robbins <robertrobbins321@msn.com>
2023-03-29 21:34:20 -0600

    Rough game board sytling set up

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import { useState } from 'react';
 
 const App = () => {
@@ -6,41 +7,81 @@ const App = () => {
 
   return (
     <div>
-      <table>
-        <tr>
-          <td>
+      <div className="game-container">
+        <div className="game-board-row">
+          <span>
+            <button className="top-square left-square">{topRight}</button>
+          </span>
+          <span>
+            <button className="top-square">{topRight}</button>
+          </span>
+          <span>
+            <button className="top-square right-square ">{topRight}</button>
+          </span>
+        </div>
+
+        <div className="game-board-row">
+          <span>
+            <button className="left-square">{topRight}</button>
+          </span>
+          <span>
             <button>{topRight}</button>
-          </td>
-          <td>
-            <button>{topRight}</button>
-          </td>
-          <td>
-            <button>{topRight}</button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button>{topRight}</button>
-          </td>
-          <td>
-            <button>{topRight}</button>
-          </td>
-          <td>
-            <button>{topRight}</button>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <button>{topRight}</button>
-          </td>
-          <td>
-            <button>{topRight}</button>
-          </td>
-          <td>
-            <button>{topRight}</button>
-          </td>
-        </tr>
-      </table>
+          </span>
+          <span>
+            <button className="right-square ">{topRight}</button>
+          </span>
+        </div>
+
+        <div className="game-board-row">
+          <span>
+            <button className="left-square bottom-square">{topRight}</button>
+          </span>
+          <span>
+            <button className="bottom-square">{topRight}</button>
+          </span>
+          <span>
+            <button className="bottom-square right-square ">{topRight}</button>
+          </span>
+        </div>
+
+        {/* <table>
+          <tbody>
+            <tr>
+              <td>
+                <button>{topRight}</button>
+              </td>
+              <td>
+                <button>{topRight}</button>
+              </td>
+              <td>
+                <button>{topRight}</button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button>{topRight}</button>
+              </td>
+              <td>
+                <button>{topRight}</button>
+              </td>
+              <td>
+                <button>{topRight}</button>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <button>{topRight}</button>
+              </td>
+              <td>
+                <button>{topRight}</button>
+              </td>
+              <td>
+                <button>{topRight}</button>
+              </td>
+            </tr>
+          </tbody>
+        </table> */}
+      </div>
     </div>
   );
 };
